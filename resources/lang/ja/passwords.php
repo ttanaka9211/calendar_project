9a515e03a47199e681a5e747083515c3ca4ba412
ttanaka9211 <ttanaka@hatchhdogs.net>
2020-12-06 17:40:24 +0900

    日本語設定

--- resources/lang/ja/passwords.php
+++ resources/lang/ja/passwords.php
@@ -1,20 +1,21 @@
 <?php
 
 return [
+
     /*
     |--------------------------------------------------------------------------
-    | Password Reminder Language Lines
+    | パスワードリセット言語行
     |--------------------------------------------------------------------------
     |
-    | The following language lines are the default lines which match reasons
-    | that are given by the password broker for a password update attempt
-    | has failed, such as for an invalid token or invalid new password.
+    | 以下の言語行は既存のパスワードを無効にしたい場合に、無効なトークンや
+    | 新しいパスワードが入力された場合のように、パスワードの更新に失敗した
+    | 理由を示すデフォルトの文言です。
     |
     */
 
-    'reset'     => 'パスワードをリセットしました。',
-    'sent'      => 'パスワードリマインダーを送信しました。',
-    'throttled' => '時間を置いて再度お試しください。',
-    'token'     => 'このパスワードリセットトークンは無効です。',
-    'user'      => 'このメールアドレスに一致するユーザーを見つけることが出来ませんでした。',
+    'reset' => 'パスワードをリセットしました。',
+    'sent' => 'パスワードリセットメールを送信しました。',
+    'token' => 'このパスワードリセットトークンは無効です。',
+    'user' => "メールアドレスに一致するユーザーは存在していません。",
+
 ];
